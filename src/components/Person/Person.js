a8e523875d65f6e9c4a5e29d86c5ffd7a542021c
Raymond <ray_chris@hotmail.it>
2019-10-01 17:50:54 +0200

    styling di List Component content

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import './Person.css';
 
 class Person extends Component {
     render () {
         return (
-            <div>
+            <div className="cardPerson">
                 <p>Mi chiamo {this.props.info.name}</p>
                 <p>E ho {this.props.info.age} anni</p>
             </div>
